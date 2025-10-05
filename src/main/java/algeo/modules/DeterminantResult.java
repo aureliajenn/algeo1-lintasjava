18ca08e3f1c539c7be6b4e396bd9fd47f82f88c4
Irvin-Tandiarrang-Sumual <irvintandiarrang23@gmail.com>
2025-10-05 17:59:05 +0700

    feat: DeterminantResult class

---
+++ src/main/java/algeo/modules/DeterminantResult.java
@@ -0,0 +1,11 @@
+package algeo.modules;
+
+public class DeterminantResult {
+    public final double value;
+    public final String steps;
+
+    public DeterminantResult(double value, String steps) {
+        this.value = value;
+        this.steps = steps;
+    }
+}
