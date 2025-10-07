788e73314f297f8e5060e45745717e1c72d27aa2
Irvin-Tandiarrang-Sumual <irvintandiarrang23@gmail.com>
2025-10-07 21:54:32 +0700

    fix: interpolasiSplinaBezierKubik plis tolong dicek...

--- src/main/java/algeo/modules/Interpolation.java
+++ src/main/java/algeo/modules/Interpolation.java
@@ -19,82 +19,78 @@ public class Interpolation {
     }
 
     public static Matrix[] interpolasiSplinaBezierKubik(Matrix points) {
-        int n = points.getRowsCount() - 1;
-        double[] X = points.getCol(0);
-        double[] Y = points.getCol(1);
-
-        // Matriks koefisien n+1 x n+1
-        Matrix A = new Matrix(n+1, n+1);
-        double[] Rx = new double[n+1];
-        double[] Ry = new double[n+1];
-
-        // kiri: 2*P1₀ + P1₁ = P₀ + 2 * P₁
-        A.setElmt(0, 0, 2);
-        A.setElmt(0, 1, 1);
-        Rx[0] = X[0] + 2 * X[1];
-        Ry[0] = Y[0] + 2 * Y[1];
-
-        // kanan: P1ₙ₋₁ + 2*P1ₙ = 2*Pₙ + Pₙ₋₁
-        A.setElmt(n, n-1, 1);
-        A.setElmt(n, n, 2);
-        Rx[n] = 2 * X[n] + X[n-1];
-        Ry[n] = 2 * Y[n] + Y[n-1];
-
-        // tengah
-        for (int i = 1; i < n; i++) {
-            A.setElmt(i, i - 1, 1);
-            A.setElmt(i, i, 4);
-            A.setElmt(i, i + 1, 1);
-            Rx[i] = 4 * X[i] + 2 * X[i+1];
-            Ry[i] = 4 * Y[i] + 2 * Y[i+1];
+        int numPoints = points.getRowsCount();
+        if (numPoints < 3) {
+            throw new IllegalArgumentException("Dibutuhkan minimal 3 titik untuk interpolasi spline.");
         }
+        int systemSize = numPoints - 2;
 
-        // solve P1
-        // Create augmented matrices properly
-        Matrix Abx = new Matrix(n+1, n+2);
-        Matrix Aby = new Matrix(n+1, n+2);
+        // Generate tridiagonal matrix
+        Matrix tridiagonal = tridiagonalMatrix(systemSize);
 
-        for (int i = 0; i < n+1; i++) {
-            for (int j = 0; j < n + 1; j++) {
-                Abx.setElmt(i, j, A.getElmt(i, j));
-                Aby.setElmt(i, j, A.getElmt(i, j));
-            }
-            Abx.setElmt(i, n + 1, Rx[i]);
-            Aby.setElmt(i, n + 1, Ry[i]);
+        // X-part
+        Matrix xPart = createVectorFromPoints(points, 0);
+
+        // Y-part
+        Matrix yPart = createVectorFromPoints(points, 1);
+
+        // Solve for x
+        Matrix xSolution = SPL.gauss(Matrix.augment(tridiagonal, xPart)).solution;
+
+        // Solve for y
+        Matrix ySolution = SPL.gauss(Matrix.augment(tridiagonal, yPart)).solution;
+
+        // Satuin kembali menjadi titik
+        Matrix[] result = new Matrix[xSolution.getRowsCount()];
+        for (int i = 0; i < xSolution.getRowsCount(); i++) {
+            double mx = xSolution.getElmt(i, 0);
+            double my = ySolution.getElmt(i, 0);
+            result[i] = new Matrix(new double[][] { { mx, my } });
         }
-
-        // solve spl pakai gauss
-        Matrix Dx = SPL.gauss(Abx).solution;
-        Matrix Dy = SPL.gauss(Aby).solution;
-
-        // bezier segments
-        Matrix[] result = new Matrix[n];
-
-        for (int i = 0; i < n; i++) {
-            double P0x = X[i];
-            double P0y = Y[i];
-            double P3x = X[i + 1];
-            double P3y = Y[i + 1];
-            double P1x = Dx.getElmt(i, 0);
-            double P1y = Dy.getElmt(i, 0);
-
-            double P2x;
-            double P2y;
-            if (i < n - 1) {
-                P2x = 2 * X[i + 1] - Dx.getElmt(i + 1, 0);
-                P2y = 2 * Y[i + 1] - Dy.getElmt(i + 1, 0);
-            } else {
-                P2x = (X[n] + Dx.getElmt(n, 0)) / 2;
-                P2y = (Y[n] + Dy.getElmt(n, 0)) / 2;
-            }
-
-            double[][] controlPoints = {
-                    {P0x, P0y}, {P1x, P1y},
-                    {P2x, P2y}, {P3x, P3y}
-            };
-            result[i] = new Matrix(controlPoints);
-        }
-
         return result;
     }
+
+    private static Matrix tridiagonalMatrix(int size) {
+        Matrix matrix= new Matrix(size, size);
+
+        for (int i = 0; i < size; i++) {
+            // set main diagonal to 4
+            matrix.setElmt(i, i, 4);
+
+            // set above main diagonal to 1
+            if (i < size - 1) {
+                matrix.setElmt(i, i + 1, 1);
+            }
+
+            // set below main diagonal to 1
+            if (i > 0) {
+                matrix.setElmt(i, i - 1, 1);
+            }
+        }
+        return matrix;
+    }
+
+    private static Matrix createVectorFromPoints(Matrix points, int col) {
+        int vectorSize = points.getRowsCount() - 2;
+
+        // Create the correctly sized result matrix (a column vector)
+        Matrix resultVector = new Matrix(vectorSize, 1);
+
+        // fill all points
+        for (int i = 0; i < vectorSize; i++) {
+            double val = 6 * points.getElmt(i + 1, col);
+            resultVector.setElmt(i, 0, val);
+        }
+
+        // adjust first 6s1 -> 6s1- s0
+        double firstVal = resultVector.getElmt(0, 0) - points.getElmt(0, col);
+        resultVector.setElmt(0, 0, firstVal);
+
+        // adjust last 6_sn-1 -> 6_sn-1 - sₙ
+        double lastVal = resultVector.getElmt(vectorSize - 1, 0) - points.getElmt(points.getRowsCount() - 1, col);
+        resultVector.setElmt(vectorSize - 1, 0, lastVal);
+
+        return resultVector;
+    }
 }
+
